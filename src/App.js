a53300c7b8a87bac1add387124586e2356cee94d
Junzhe Piao <junzhepiao@gmail.com>
2019-02-19 23:44:50 -0700

    e-mail inbox application with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,156 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import MessageList from './MessageList';
+import Toolbar from './Toolbar';
+import axios from 'axios';
+import ComposeForm from './ComposeForm'
 class App extends Component {
+  state = {
+    messages: [],
+    showCompose: false
+  }
+  componentDidMount = async ()=>{
+    let messages = await axios.get (`http://localhost:8000/messages`)
+    this.setState({messages:messages.data})
+  }
+  addMessage=async(message)=>{
+    let newMessage = {
+      ...message,
+      labels: JSON.stringify({}),
+      read:false,
+      selected:false,
+      starred:false
+    }
+  let newMessages = await axios.post(`http://localhost:8000/messages`, newMessage)
+  this.setState({
+    messages: newMessages.data
+  })
+}
+  toggleComposeForm=()=>{
+    this.setState({
+      showCompose:!this.state.showCompose
+    })
+  }
+  toggleRead =(selectedMessage)=>{
+    let otherMessages = this.state.messages.filter(message => selectedMessage.id !=message.id)
+    let changedMessages = {
+      id: selectedMessage.id,
+      subject: selectedMessage.subject,
+      read: !selectedMessage.read,
+      starred: selectedMessage.starred,
+      labels: selectedMessage.labels
+    }
+    this.setState({messages: otherMessages.concat(changedMessages).sort((a,b)=> a.id - b.id)})
+  }
+  toggleStarred =(selectedMessage)=>{
+    let otherMessages = this.state.messages.filter(message => selectedMessage.id !=message.id)
+    let changedMessages = {
+      id: selectedMessage.id,
+      subject: selectedMessage.subject,
+      read: selectedMessage.read,
+      starred: !selectedMessage.starred,
+      labels: selectedMessage.labels,
+      selected: selectedMessage.selected
+    }
+    this.setState({messages: otherMessages.concat(changedMessages).sort((a,b)=> a.id - b.id)})
+  }
+  toggleSelected =(selectedMessage)=>{
+    let otherMessages = this.state.messages.filter(message => selectedMessage.id !=message.id)
+    let changedMessages = {
+      id: selectedMessage.id,
+      subject: selectedMessage.subject,
+      read: selectedMessage.read,
+      starred: selectedMessage.starred,
+      labels: selectedMessage.labels,
+      selected: !selectedMessage.selected || false
+    }
+    this.setState({messages: otherMessages.concat(changedMessages).sort((a,b)=> a.id - b.id)})
+  }
+  selectButtonFunc =(type)=> {
+    if(type.indexOf('-minus')){
+      this.setState({
+        messages: this.state.messages.map(msg=>{
+          msg.selected = true
+          return msg
+        })
+      })
+    } else if (type.indexOf('-check')){
+      this.setState({
+        messages:this.state.messages.map(msg=>{
+          msg.selected=false
+          return msg
+        })
+      })
+    } else{
+      this.setState({
+        messages:this.state.messages.map(msg=>{
+          msg.selected=true
+          return msg
+        })
+      })
+    }
+  }
+
+setUnreadFunc =() =>{
+  let newState=this.state.messages.map(msg=>{
+    if(msg.selected)msg.read = false
+    return msg;
+  })
+  this.setState({
+    messages:newState
+  })
+}
+setReadFunc =() =>{
+  let newState=this.state.messages.map(msg=>{
+    if(msg.selected)msg.read = true
+    return msg;
+  })
+  this.setState({
+    messages:newState
+  })
+}
+deleteMessages =() =>{
+  let newState=this.state.messages.filter(msg=>!msg.selected)
+  this.setState({
+    messages:newState
+  })
+}
+addLabel=(label)=>{
+  let newState= this.state.messages.map(msg=>{
+    if(msg.selected && !msg.labels.includes(label))msg.labels.push(label)
+    return msg
+  })
+  this.setState({messages:newState})
+}
+removeLabel=(label)=>{
+  let newState= this.state.messages.map(msg=>{
+    if(msg.selected)msg.labels=msg.labels.filter(l=>l !==label)
+  return msg
+  })
+  this.setState({messages:newState})
+}
   render() {
+    let numOfSelectedMsgs = this.state.messages.filter(msg=> msg.selected).length;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Toolbar 
+      numOfSelectedMsgs={numOfSelectedMsgs} 
+      messages={this.state.messages} 
+      selectButtonFunc={this.selectButtonFunc}
+      setUnreadFunc={this.setUnreadFunc}
+      setReadFunc={this.setReadFunc}
+      deleteMessages={this.deleteMessages}
+      addLabel={this.addLabel}
+      removeLabel={this.removeLabel}
+      toggleComposeForm={this.toggleComposeForm}
+      showCompose={this.state.showCompose}
+      />
+      {this.state.showCompose && <ComposeForm addMessage={this.addMessage}/>}
+     
+      <MessageList 
+      messages={this.state.messages} 
+      toggleRead={this.toggleRead} 
+      toggleStarred={this.toggleStarred} 
+      toggleSelected={this.toggleSelected}/>
       </div>
     );
   }
---
+++ src/Toolbar.js
@@ -0,0 +1,82 @@
+import React,{Component} from 'react'
+
+class Toolbar extends Component {
+    render(){
+        let selectButtonClass = 'fa-square-o';
+        let messagesSelected = this.props.messages.filter(message=> message.selected);
+        if (messagesSelected.length === this.props.messages.length) {
+            selectButtonClass = "fa-check-square-o";
+        } else if (messagesSelected[0]){
+            selectButtonClass = "fa-minus-square-o";
+        }
+        // let someSelected = this.props.numOfSelectedMsgs > 0 && this.props.numOfSelectedMsgs < this.props.messages.length ? '-minus' : null;
+        // let allSelected = this.props.numOfSelectedMsgs < this.props.messages.length ? '-check' : null;
+        // let noneSelected = this.props.numOfSelectedMsgs === 0 ? '': null;
+        let countedUnread = this.props.messages.filter(msg=>!msg.read).length;
+        let countedSelected = this.props.messages.reduce((acc, val)=>
+         acc + !!val.selected,0)
+        
+        
+        return (
+            <div className="row toolbar">
+                <div className="col-md-12">
+                    <p className="pull-right">
+                    <span className="badge badge">{countedUnread}</span>
+                    {countedUnread > 1 || countedUnread < 1 ? 'unread messages' : 'unread message'}
+                    </p>
+
+                    <button className="btn btn-danger" onClick={ this.props.toggleComposeForm}>
+                    <i className="fa fa-plus"></i>
+                    </button>
+
+                    <button className="btn btn-default" 
+                    onClick={()=> this.props.selectButtonFunc(selectButtonClass)}
+                    disabled={!countedSelected}>
+                    <i className={`fa ${selectButtonClass}`}
+                    ></i>
+                    </button>
+
+                    <button className="btn btn-default"
+                     onClick={()=> this.props.setReadFunc()}
+                     disabled={!countedSelected}
+                    >
+                    Mark As Read
+                    </button>
+
+                    <button className="btn btn-default"
+                    onClick={()=> this.props.setUnreadFunc()}
+                    disabled={!countedSelected}>
+                    Mark As Unread
+                    </button>
+
+                    <select className="form-control label-select"
+                    onChange={(e)=> this.props.addLabel(e.target.value)}
+                    disabled={!countedSelected}>
+                    <option>Apply label</option>
+                    <option value="dev">dev</option>
+                    <option value="personal">personal</option>
+                    <option value="gschool">gschool</option>
+                    </select>
+
+                    <select className="form-control label-select"
+                    onChange={(e)=> this.props.removeLabel(e.target.value)}
+                    disabled={!countedSelected}>
+                    <option>Remove label</option>
+                    <option value="dev">dev</option>
+                    <option value="personal">personal</option>
+                    <option value="gschool">gschool</option>
+                    </select>
+
+                    <button className="btn btn-default" onClick={()=> this.props.deleteMessages()}
+                    disabled={!countedSelected}>
+                    <i className="fa fa-trash-o"></i>
+                    </button>
+                </div> 
+                
+            </div>
+
+        )
+    }
+}
+
+export default Toolbar
